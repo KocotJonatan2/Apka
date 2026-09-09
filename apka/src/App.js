84ac38df939e84bc9f566953e951db14b0a6f655
Jonatan Kocot <Kocotjonatan2@gmail.com>
2026-09-09 14:17:47 +0200

    dodalem zdjecie

--- apka/src/App.js
+++ apka/src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import zje from "./12.jpg";
+import zje2 from "./gra.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="portfolio">
+      <header className="top">
+        <div className="photo-box">
+            <img src={zje} alt="Moje zdjęcie" />
+        </div>
+
+        <div className="exams">
+          <h2>Egzaminy</h2>
+
+          <div className="exam">
+            <span>INF.03</span>
+            <strong>89%</strong>
+          </div>
+
+          <div className="exam">
+            <span>INF.04</span>
+            <strong>100%</strong>
+          </div>
+        </div>
       </header>
+
+      <section className="projects">
+        <h1>Moje projekty</h1>
+
+        <div className="projects-grid">
+
+          <div className="project">
+            <div className="project-image">
+              <img src={zje2} alt="Projekt 1" id="project1" />
+            </div>
+
+            <div className="project-content">
+              <h3>Gra komputerowa</h3>
+              <p>
+                Moja pierwsza gra komputerowa.
+              </p>
+
+            </div>
+          </div>
+
+          <div className="project">
+            <div className="project-image">
+              <img src={zje2} alt="Projekt 1" id="project1" />
+            </div>
+
+            <div className="project-content">
+              <h3>Gra komputerowa</h3>
+              <p>
+                Moja pierwsza gra komputerowa.
+              </p>
+
+            </div>
+          </div>
+
+          <div className="project">
+            <div className="project-image">
+              <img src={zje2} alt="Projekt 1" id="project1" />
+            </div>
+
+            <div className="project-content">
+              <h3>Gra komputerowa</h3>
+              <p>
+                Moja pierwsza gra komputerowa.
+              </p>
+
+            </div>
+          </div>
+
+          
+
+        </div>
+      </section>
     </div>
   );
 }
